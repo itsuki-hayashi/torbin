63c36fce64f850af22ecbce95eb1124c616e2f25
Zhiyuan Lin <2121340+zhiyuan-lin@users.noreply.github.com>
2021-07-03 03:16:58 +0900

    Improve code e.g. Webmozart assertions (#9)

--- app/routes.php
+++ app/routes.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
-use DI\Container;
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -14,7 +14,7 @@ use Webmozart\Assert\Assert;
 
 return function (App $app): void {
     $container = $app->getContainer();
-    Assert::notNull($container, '$container is null.');
+    Assert::isInstanceOf($container, ContainerInterface::class);
 
     $app->options('/{routes:.*}', function (Request $request, Response $response): Response {
         // CORS Pre-Flight OPTIONS Request Handler
